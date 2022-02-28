2f16d641bd351d3943d4c87828fe2b373437fe71
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-02-28 06:58:50 +0100

    add image upload service & controller

--- src/main/java/com/othmane/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/othmane/awsimageupload/profile/UserProfileController.java
@@ -1,11 +1,11 @@
 package com.othmane.awsimageupload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -21,5 +21,9 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId, @RequestParam("file") MultipartFile file) {
+        this.userProfileService.uploadUserProfileImage(userProfileId,file);
     }
 }
--- src/main/java/com/othmane/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/othmane/awsimageupload/profile/UserProfileService.java
@@ -1,9 +1,13 @@
 package com.othmane.awsimageupload.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -18,4 +22,11 @@ public class UserProfileService {
         return userProfileDataAccessService.getUserProfiles();
     }
 
+    @PostMapping(
+            path = "{userProfileId}/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+
+    }
 }
