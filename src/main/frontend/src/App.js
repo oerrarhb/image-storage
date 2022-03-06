803d44a04b30e23e54aa7f954b1f4a03061f624d
Othmane ERRARHBI <othmane.errarhbi@deveryware.com>
2022-03-06 16:50:59 +0100

    add react drop zone

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect,useCallback} from 'react';
 import './App.css';
 import axios from 'axios';
+import Dropzone, {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () =>
 {
@@ -21,8 +22,12 @@ const UserProfiles = () =>
     {
       return (
         <div key={index}>
+          <br/>
+          <br/>
           <h1>{userProfile.username}</h1>
           <p>{userProfile.userProfileId}</p>
+          <MyDropzone/>
+          <br/>
         </div>
       )
     });
@@ -34,6 +39,25 @@ function App() {
         <UserProfiles/>
     </div>
   );
+}
+
+function MyDropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profile image, or click to select profile image</p>
+      }
+    </div>
+  )
 }
 
 export default App;
